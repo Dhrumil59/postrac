dc6e98e4edc36f8c9f1f8ab02fe12fabbe2bc0d7
Dhrumil59 <dhrumilshah593@gmail.com>
2023-04-02 13:08:32 +0530

    Upadte all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,202 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import './App.css';
+// import React, {useReducer, useRef} from 'react';
+// import * as tf from "@tensorflow/tfjs";
+// import * as posenet  from '@tensorflow-models/posenet';
+// import Webcam from 'react-webcam';
+// import {drawKeypoints, drawSkeleton} from "./utilities"; 
+// function App() {
+//   const webcamRef = useRef(null);
+//   const canvasRef = useRef(null); 
+//   // posenet load 
+//   const runPosenet = async()=>{
+//       const net = await posenet.load(
+//         {
+//           inputResolution:{width:640 , height:480}, 
+//           scale :0.5
+//         });
+//         // intervla 
+//         setInterval (() => {
+//           detect(net);
+//         },100 );
+      
+
+//   }
+//   const detect = async (net)=>{
+//     if (typeof webcamRef.current!== "undefined"&&webcamRef.current!==null && webcamRef.current.video.readyState===4){
+//       // get properties 
+//       const video = webcamRef.current.video;
+//       const videoWidth = webcamRef.current.video.videoWidth; 
+//       const videoHeight = webcamRef.current.video.videoHeight; 
+
+//       // videowidth 
+//         webcamRef.current.video.width = videoWidth;
+//         webcamRef.current.video.header= videoHeight;
+
+//       // detect 
+//       const pose = await net.estimateSinglePose(video);
+//       console.log(pose);
+
+//       drawCanvas = (pose,video,videoWidth,videoHeight, canvasRef);
+//     }
+//   };  
+//   const drawCanvas = (pose,video,videoWidth,videoHeight, canvas) =>{
+//     const ctx = canvas.current.getContext("2d");
+//     canvas.current.width =videoWidth;
+//     canvas.current.height=videoHeight;
+
+//     drawKeypoints(pose["keypoints"],0.6,ctx);
+//     drawSkeleton(pose["keypoints"],0.7,ctx);
+//   };
+//    runPosenet();
+//   return (
+//     <div className="App">
+
+//       <header className="App-header">
+//        <Webcam
+//           ref = {webcamRef}
+//            style = {
+//                     {   
+//                       position:"absolute",
+//                       margin: "auto",
+//                       marginRight: "auto", 
+//                       left:0,right:0, 
+//                       textAlign:'center',
+//                       zIndex: 9,
+//                       width: 640,
+//                       height: 480,
+                      
+//                     }
+                      
+//         }/>
+//         <canvas
+//           ref = {canvasRef}
+//            style = {
+//                     {   
+//                       position:"absolute",
+//                       margin: "auto",
+//                       marginRight: "auto", 
+//                       left:0,right:0, 
+//                       textAlign:'center',
+//                       zIndex: 9,
+//                       width: 640,
+//                       height: 480,
+                      
+//                     }
+                      
+//         }/>
+       
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// 1. Install dependencies DONE
+// 2. Import dependencies DONE
+// 3. Setup webcam and canvas DONE
+// 4. Define references to those DONE
+// 5. Load posenet DONE
+// 6. Detect function DONE
+// 7. Drawing utilities from tensorflow DONE
+// 8. Draw functions DONE
+
+import React, { useRef } from "react";
+import "./App.css";
+import * as tf from "@tensorflow/tfjs";
+import * as posenet from "@tensorflow-models/posenet";
+import Webcam from "react-webcam";
+import { drawKeypoints, drawSkeleton } from "./utilities";
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  //  Load posenet
+  const runPosenet = async () => {
+    const net = await posenet.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 0.4,
+    });
+    //
+    setInterval(() => {
+      detect(net);
+    }, 20);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Make Detections
+      const pose = await net.estimateSinglePose(video);
+      console.log(pose);
+
+      drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+    }
+  };
+
+  const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
+    const ctx = canvas.current.getContext("2d");
+    canvas.current.width = videoWidth;
+    canvas.current.height = videoHeight;
+
+    drawKeypoints(pose["keypoints"], 0.2, ctx);
+    drawSkeleton(pose["keypoints"], 0.2, ctx);
+  };
+
+  runPosenet();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
       </header>
     </div>
   );
 }
 
 export default App;
+
